ceb5e7fa371a9adc44d3cda6869e47496c9a928a
philw <philw@210ab51a-7514-0410-b6e0-de9c396846df>
2012-08-06 09:24:02 +0000

    Missing for some reason since svn schema change

---
+++ trunk/interfaces/web/js/biblioData.js
@@ -0,0 +1,24 @@
+/* Handles populating the form fields for biblio references data selected from list */
+
+function populateBibDetails(userString,refNo){
+    // alert("User string="+ userString);
+    var q = $.parseQuery(userString);
+    $('#title').val(q.title);
+    $('#authors1').val(q.author1 + ", " + q.author2);
+    $('#journal').val(q.journal);
+    $('#year').val(q.year);
+    $('#volume').val(q.volume);
+    $('#pages').val(q.pages);
+}
+function removeBibDetails(){
+    $('#pubmed_id').val(null);
+    $('#reference_descr').val(null);
+    $('#notes').val(null);
+    $('#title').val(null);
+    $('#authors1').val(null);
+    $('#journal').val(null);
+    $('#year').val(null);
+    $('#volume').val(null);
+    $('#pages').val(null);
+}
+
