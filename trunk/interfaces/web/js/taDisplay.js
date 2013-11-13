a490173683008d5631b72cf7d5e89444934f7a5b
philw <philw@210ab51a-7514-0410-b6e0-de9c396846df>
2013-11-13 14:30:52 +0000

    Correcting URL to point to base url for emma service charge.

---
+++ trunk/interfaces/web/js/taDisplay.js
@@ -0,0 +1,79 @@
+var selectedElement;
+       jQuery(this).flushCache();
+function getListValue() {
+    selectedElement = document.forms[0].con_country.value;
+    
+    searchArray();
+}
+function chkCountry(country) {
+    getListValue();
+    selectedElement = country;
+    //alert(selectedElement);
+    searchArray();
+}
+
+function ajaxDisplay(display) {
+    
+    $.ajax({
+        type: "POST",
+        url: "../ajaxReturn.emma",
+        data: "display=" + display,
+        cache: false,
+        success: function(msg){
+             //alert( "Data Saved: " + msg );
+            $("#taOptions").html(msg);
+        }
+    });
+}
+
+var euCountriesList = ['Austria','Belgium','Bulgaria','Cyprus','Czech Republic','Denmark','Estonia','Finland','France','Germany','Greece','Hungary','Ireland','Italy','Latvia','Lithuania','Luxembourg','Malta','Netherlands','Poland','Portugal','Romania','Slovakia','Slovenia','Spain','Sweden','United Kingdom'];
+var assocCountriesList = ['Albania','Croatia','Iceland','Israel','Liechtenstein','Macedonia','Montenegro','Norway','Serbia','Switzerland','Turkey'];
+
+
+function searchArray(){
+    //SEARCH OVER ALL ELEMENTS
+    var id;
+    var index;// = euCountriesList.indexOf(selectedElement)
+    for (id in euCountriesList) {
+        
+        if (euCountriesList[id] == selectedElement) {
+            index = 1;
+            break;
+        } else {
+            index=-1;
+        }
+    }
+    if (index == -1) {
+        //index = assocCountriesList.indexOf(selectedElement)
+        var id;
+        for (id in assocCountriesList) {
+            if (assocCountriesList[id] == selectedElement) {
+                index = 1;
+                break;
+            } else {
+                index=-1;
+            }
+        }
+    }
+    if (index != -1)
+    {
+        //allow display
+        // show('taOptions');
+        show('vat');
+        document.forms[0].eligible_country.value="yes";
+        
+        //call ajax function
+        ajaxDisplay("enabled");
+        
+    } else if (index == -1) {
+        //    hide('taOptions');
+        hide('vat');
+       // hide('taProjDesc');
+        document.forms[0].eligible_country.value="no";
+        
+        //call ajax function
+        ajaxDisplay("disabled");
+    }
+}
+
+
