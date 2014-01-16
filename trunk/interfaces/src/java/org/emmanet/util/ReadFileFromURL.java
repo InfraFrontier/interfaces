3634f91cbad626a14845be0820b1664651ca837b
philw <philw@210ab51a-7514-0410-b6e0-de9c396846df>
2014-01-16 17:28:15 +0000

    Changes to address EMMA-539 and EMMA-531. To resolve MTA attachment a new class had to be implemented to read file from url as no access to file location from tomcat vm. File gets written to tmp then deleted once message is sent.
    539 almost resolved but need to display and include input from vat field.

---
+++ trunk/interfaces/src/java/org/emmanet/util/ReadFileFromURL.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.emmanet.util;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author philwilkinson
+ */
+public class ReadFileFromURL {
+
+    private URL url;
+    final static String TMPFILES = Configuration.get("TMPFILES");
+
+    public File ReadFileURL(URL fileurl, String filename) {
+
+        String fileName = filename;
+        File file = new File(fileName);
+        try {
+            byte[] buffer = new byte[8 * 1024];
+            // get URL content
+            url = new URL(fileurl.toString());
+            URLConnection conn = url.openConnection();
+
+            InputStream input = conn.getInputStream();
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            OutputStream os = new FileOutputStream(file.getAbsoluteFile());
+            int bytesRead;
+            while ((bytesRead = input.read(buffer)) != -1) {
+                os.write(buffer, 0, bytesRead);
+            }
+            os.close();
+            input.close();
+        } catch (MalformedURLException e) {
+            Logger.getLogger(Encrypter.class.getName()).log(Level.SEVERE, null, e);
+        } catch (IOException e) {
+            Logger.getLogger(Encrypter.class.getName()).log(Level.SEVERE, null, e);
+        }
+        return file;
+    }
+}
