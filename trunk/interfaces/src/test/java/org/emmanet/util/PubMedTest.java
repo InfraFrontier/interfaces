dd9e59533a01e458955452da4503527d04a810f4
philw <philw@210ab51a-7514-0410-b6e0-de9c396846df>
2014-02-20 14:17:16 +0000

    Argh another failure on expected return from pubmed. After being changed to caps for every word in title this has now reverted back to lc in pubmed record causing test failure

--- trunk/interfaces/src/test/java/org/emmanet/util/PubMedTest.java
+++ trunk/interfaces/src/test/java/org/emmanet/util/PubMedTest.java
@@ -131,7 +131,7 @@ public class PubMedTest {
         assertEquals("415-427", instance.getPages());
         assertEquals("", instance.getPaperid());
         assertEquals("24248598", instance.getPubmedId());
-        assertEquals("Regulation of NO Synthesis, Local Inflammation, and Innate Immunity to Pathogens by BET Family Proteins.", instance.getTitle());
+        assertEquals("Regulation of NO synthesis, local inflammation, and innate immunity to pathogens by BET family proteins.", instance.getTitle());
         assertEquals("34", instance.getVolume());
         assertEquals(2014, instance.getYear());
     }
