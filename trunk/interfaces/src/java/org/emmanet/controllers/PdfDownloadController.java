02d863b3b34595935046ef64cbea2e9b555df81a
philw <philw@210ab51a-7514-0410-b6e0-de9c396846df>
2014-04-14 15:51:09 +0000

    New controller and view to securely display uploaded files from the new submission form in the strainsUpdateInterface.
    Now uses Spring Security to control access.
    
    Resolves EMMA-361

---
+++ trunk/interfaces/src/java/org/emmanet/controllers/PdfDownloadController.java
@@ -0,0 +1,70 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.emmanet.controllers;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.emmanet.util.Configuration;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+/**
+ *
+ * @author phil
+ */
+public class PdfDownloadController extends AbstractController {
+
+    final static String SUBFORMUPLOAD = Configuration.get("SUBFORMUPLOAD");
+
+    @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        // @RequestMapping(value = "/download" , method = RequestMethod.GET) public void doDownload(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        ServletContext context = request.getSession().getServletContext();
+        String filename = request.getParameter("filename");
+        filename = URLDecoder.decode(filename, "UTF-8");
+        
+        System.out.println("DECODED FILENAME == " + filename);
+        
+        String filePath = SUBFORMUPLOAD;
+        String fullPath = filePath + filename;
+        File downloadFile = new File(fullPath);
+        FileInputStream inputStream = new FileInputStream(downloadFile);
+        // get MIME type of the file
+        String mimeType = context.getMimeType(fullPath);
+        if (mimeType == null) {
+            // set to binary type if MIME mapping not found
+            mimeType = "application/pdf";
+        }
+        System.out.println("MIME type: " + mimeType);
+        response.setContentType("application/pdf");
+
+        // get output stream of the response
+        OutputStream outputStream = response.getOutputStream();
+
+        byte[] buffer = new byte[1024];
+        int bytesRead = -1;
+        // write bytes read from the input stream into the output stream
+        try {
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (outputStream != null) {
+                outputStream.close();
+            }
+        }
+        return null;
+    }
+}
